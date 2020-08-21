301486da242673497746e5613a632749aa8ac73c
jonatas alves <jonatas101@hotmail.com>
2020-08-20 22:16:37 -0300

    adicionada apresentação das ceramicas em Cards()

--- lib/ceramicas.dart
+++ lib/ceramicas.dart
@@ -1,32 +1,27 @@
 import 'package:flutter/material.dart';
 
-class CeramicaWidget extends StatelessWidget{
-
-  CeramicaWidget(this.name, this.imageSrc):super();
+class CeramicaWidget extends StatelessWidget {
+  CeramicaWidget(this.name, this.imageSrc) : super();
 
   final String name;
   final String imageSrc;
 
   @override
-  Widget build(BuildContext context){
-    return Padding(
-      padding: EdgeInsets.all(20.0),
-      child: Container(
-        decoration: BoxDecoration(border: Border.all()),
-        padding: EdgeInsets.all(20.0),
-
-      child: Center(
-                child: Column(
-                  children: <Widget>[
-                    Text('${name}', style: TextStyle(fontSize: 24.0)),
-                    Padding(
-                      padding: EdgeInsets.only(top:20.0),
-                      child: Image.network(imageSrc)
-                    )
-                  ]
-                )
-      )
-      )
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        semanticContainer: true,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: Image.network(
+          this.imageSrc,
+          fit: BoxFit.fill,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        elevation: 5,
+        margin: EdgeInsets.all(10),
+      ),
     );
   }
 }

2d51a5a3cb6f2f6ca4b23fffde8a7a84709808c2
1810TEC01 <1810TEC01@ei.esmalglass-itaca.com>
2020-08-27 17:19:15 -0300

    montando Alert com um row e om dropdown list

--- lib/novoPedidoWidget.dart
+++ lib/novoPedidoWidget.dart
@@ -1,16 +1,58 @@
 import 'package:flutter/material.dart';
 
 showAlertDialog(BuildContext context){
-  Widget okButton = FlatButton(
-    child: Text("OK"),
+  final _formKey = GlobalKey<FormState>();
+  Widget saveButton = FlatButton(
+    child: Text("save"),
     onPressed: null,
   );
 
   AlertDialog alert = AlertDialog(
-    title: Text("Novo Pedido"),
-    content: Text("This is the content"),
+    title: Text("Novo Pedido",
+                style: TextStyle(color: Colors.blue[900])
+              ),
+    content: Row(
+      children:<Widget>[
+        Expanded(
+          child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: 'Número do Pedido',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),//TextFormField
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: RaisedButton(
+                onPressed: () {
+                  // Validate will return true if the form is valid, or false if
+                  // the form is invalid.
+                  if (_formKey.currentState.validate()) {
+                    // Process data.
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ),
+          ],//colum/children
+        ),//form/column
+      ),//form
+        ),
+      ],
+),//row
+
     actions: [
-      okButton,
+      saveButton,
     ],
   );
 
